d50cae4925693cbcfed8a50ba4bba6182a3c5580
João F.S.E <jofisaes@hotmail.com>
2019-05-16 17:37:03 +0200

    solution update

--- server.js
+++ server.js
@@ -3,12 +3,15 @@
 
 // init project
 var express = require('express');
+var parser = require('body-parser');
+
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(parser.urlencoded({extended: false}));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -24,7 +27,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/timestamp/:date_string?", (req, res)=>{
+  let dateString = req.params.date_string;
+  let parseDate = dateString ? new Date() : new Date(dateString);
+  if(parseDate.toUTCString() === 'Invalid Date'){
+    res.json({"error" : "Invalid Date" });
+  } else {
+    res.json({"unix":parseDate.getTime(), "utc":parseDate.toUTCString()}); 
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
